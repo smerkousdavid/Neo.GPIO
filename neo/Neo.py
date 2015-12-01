1f683fb3dcfae2956dfb68dbb309d40db811342d
David Smerkous <smerkousdavid@gmail.com>
2015-12-01 22:00:14 +0000

    just better

--- neo/Neo.py
+++ neo/Neo.py
@@ -9,33 +9,40 @@ except ImportError:
 	system("apt-get install python-numpy python-scipy ")
 class Neo:
 	def __init__(self):
-		self.gpios = ["178", "179", "104", "143", "142", "141", "140", "149", "105", "148", "146", "147", "100", "102"]
-		self.called = ""
-		self.pwms = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]
+		self.gpios = ["178", "179", "104", "143", "142", "141", "140", "149", "105", "148", "146", "147", "100", "102", "102", "106", "106", "107", "180", "181", "172", "173", "182", "124",
+		"25", "22", "14", "15", "16", "17", "18", "19", "20", "21", "203", "202", "177", "176", "175", "174", "119", "124", "127", "116", "7", "6", "5", "4"]
+		self.gpioval = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		self.gpiodir = self.gpioval
+		current = 0
 		for num in self.gpios:
 			try:
-				f = open("/sys/class/gpio/export", "w")
-				f.write(str(num))
-				f.close()
+				with open("/sys/class/gpio/export", "w") as create
+					create.write(num)
+				with open("/sys/class/gpio/gpio"+self.gpios[current]+"/value", "r") as reads:
+					self.gpioval[current]=reads.read()
+				with open("/sys/class/gpio/gpio"+self.gpios[current]+"/direction", "r") as readdir:
+					self.gpiodir[current] = (1 if "out" in readdir.read() else 0)
+				current++
 			except:
-				sleep(0.0001)
+				sleep(0.000001)
 		print "Neo gpios started, make sure arduino isn't using the same pins or you can ruin this board!"
 
 	def pinMode(self, pin=2, direction=0):
 		try:
-			toWrite = ""
-			if int(direction) == 0:
-				toWrite = "in"
-			elif int(direction) == 1:
-				toWrite = "out"
-			f = open("/sys/class/gpio/gpio"+self.gpios[int(pin)]+"/direction", "w")
-			f.write(toWrite)
-			f.close()
+			gpio = self.gpio[int(pin)]
+			if int(direction) != self.gpiodir[pin]:
+				with open("/sys/class/gpio/gpio"+gpio+"/direction", "w") as writer:
+					writer.write("in" if direction < 1 else "out")
+				self.gpiodir[current] = (0 if direction < 1 else 1)
 			return True
 		except ValueError:
 			print "ERROR: pinMode, value inserted wasn't an int"
 			return False
-
+		except:
+			print "ERROR: pinMode, error using pinMode"
+			return False
+			
+	'''
 	def pwmdonttouch(self, p, d):
 		try:
 			while True:
@@ -67,19 +74,39 @@ class Neo:
 		except ValueError:
 			print "ERROR: pwmWrite, value inserted wasn't an int"
 			return False
-			
+	'''		
 
 	def digitalWrite(self, pin=2, value=0):
 		try:
-			with open("/sys/class/gpio/gpio"+self.gpios[int(pin)]+"/value", "r") as reads:
-				check = reads.read()
-			if "in" in check:
-				print "Current pin direction is in"
-				return False
-			f = open("/sys/class/gpio/gpio"+self.gpios[int(pin)]+"/value", "w")
-			f.write(str(int(value)))
-			f.close()
+			gpio = self.gpios[int(pin)]
+			if self.gpiodir[pin] != 1:
+				with open("/sys/class/gpio/gpio"+gpio+"/direction", "w") as re:
+					re.write("out")
+				self.gpiodir[pin] = 1
+			if self.gpioval[pin] != int(value):
+				with open("/sys/class/gpio/gpio"+gpio+"/value", "w") as writes:
+					writes.write("0" if value < 1 else "1")
+				self.gpioval[pin] = (0 if value < 1 else 1)
 			return True
 		except ValueError:
 			print "ERROR: digitalWrite, value inserted wasn't an int"
 			return False
+		except:
+			print "ERROR: digitalWrite, error running"
+			return False
+
+	def digitalRead(self, pin=2):
+		try:
+			gpio = self.gpios[int(pin)]
+			if self.gpiodir[pin] != 0:
+				with open("/sys/class/gpio/gpio"+gpio+"/direction", "w") as re:
+					re.write("in")
+				self.gpiodir[pin] = 0
+			with open("/sys/class/gpio/gpio"+gpio+"/value", "r") as reader:
+				self.gpioval[pin] = int(reader.read().replace('\n',''))
+			return self.gpioval[pin]
+		except ValueError:
+			print "ERROR: digitalRead, value inserted wasn't an int"
+			return 0
+		except:
+			print "ERROR: digitalRead, error running"
