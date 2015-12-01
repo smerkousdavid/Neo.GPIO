a0cb86c9ebd2235acba37767a4dc8bee325e32df
David Smerkous <smerkousdavid@gmail.com>
2015-12-01 21:00:17 +0000

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+'''
+Welcome to the Neo.GPIO library created by David Smerkous, this allows easy/simple controls
+for the Udoo neo, that's as easy as it would be on the Rasberry Pi to control the GPIO pins on tge
+Neo
+
+GNU public license v2.0
+'''
+from Neo import Neo
