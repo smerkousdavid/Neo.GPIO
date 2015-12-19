d8431aee84ed537d84b43e450a011ad95bd8c671
David Smerkous <smerkousdavid@gmail.com>
2015-12-19 21:30:06 +0000

    call instead of system

--- neo/Neo.py
+++ neo/Neo.py
@@ -1,4 +1,5 @@
 from os import system, devnull
+from subprocess import call, STDOUT
 from sys import exit
 from time import sleep
 from threading import Thread
@@ -111,22 +112,43 @@ class Gpio:
 			print "ERROR: digitalRead, error running"
 			return -1
 
+class Led:
+	def __init__(self):
+		self.led = 0
+
+	def on(self):
+		with open("/sys/class/leds/led0/brightness", "w") as w:
+			w.write("1")
+
+	def off(self):
+		with open("/sys/class/leds/led0/brightness", "w") as w:
+			w.write("0")
+
 class Temp:
 	def __init__(self): # Start temp module on object call
 		self.temp = 0000
+		NULLS = open(devnull, 'w')
 		try:
-			system("rmmod lm75 > /dev/null 2>&1;modprobe lm75 > /dev/null 2>&1") # Reset the temp module/kernel i2c reset
+			call(["rmmod", "lm75"], stdout=NULLS, stderr=STDOUT) # Reset module
 		finally:
 			sleep(0.0001) # wait for script update
 		try:
-			system("sh -c 'echo lm75 0x48 >/sys/class/i2c-dev/i2c-1/device/new_device' > /dev/null 2>&1") #easier to run command to black hole using system
+			call(["modprobe", "lm75"], stdout=NULLS, stderr=STDOUT)
+		finally:
+			sleep(0.0001) # again script update		
+		try:
+			call(["echo", "lm75", "0x48", ">/sys/class/i2c-dev/i2c-1/device/new_device"], stdout=NULLS, stderr=STDOUT) #easier to run command to black hole using system
 		finally:
 			sleep(0.001) # longer script wait
 
 	def getTemp(self, mode="f"): # Return with mode
-		with open("/sys/class/i2c-dev/i2c-1/device/1-0048/temp1_input", "r") as reader: # Read i2c millicel file
-			self.temp = (float(reader.read().replace(' ','').replace('\n', '')))*(0.001) # Turn into celcius
-		return ((self.temp)*1.8+32) if "f" in mode else (self.temp) # Either return into Far or Celc
+		try:
+			with open("/sys/class/i2c-dev/i2c-1/device/1-0048/temp1_input", "r") as reader: # Read i2c millicel file
+				self.temp = (float(reader.read().replace(' ','').replace('\n', '')))*(0.001) # Turn into celcius
+		except:
+			print "Snap in sensor is not plugged in!"
+		finally:
+			return ((self.temp)*1.8+32) if "f" in mode else (self.temp) # Either return into Far or Celc
 
 class Barometer:
 	def __init__(self): 
@@ -134,25 +156,38 @@ class Barometer:
 		self.Tempscale = 0000
 		self.pressure = 0000
 		self.Tempress = 000
+		NULLS = open(devnull, 'w')
 		try:
-			system("rmmod mpl3115 > /dev/null 2>&1;modprobe mpl3115> /dev/null 2>&1") # Reset the temp module/kernel i2c reset
+			call(["rmmod", "mpl3115"], stdout=NULLS, stderr=STDOUT) # Reset module
 		finally:
 			sleep(0.0001) # wait for script update
+		try:
+			call(["modprobe", "mpl3115"], stdout=NULLS, stderr=STDOUT)
+		finally:
+			sleep(0.0001) # again script update		
 
 	def getTemp(self, mode="f"): # Return from Barometer
-		with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_raw", "r") as treader:
-			self.temp = (float(treader.read().replace('\n', '')))
-		with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_scale", "r") as tsreader:
-			self.Tempscale = (float(tsreader.read().replace('\n', '')))
-		self.temp = ((self.temp)*(self.Tempscale))
-		return ((self.temp)*1.8+32) if "f" in mode else (self.temp)
+		try:
+			with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_raw", "r") as treader:
+				self.temp = (float(treader.read().replace('\n', '')))
+			with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_scale", "r") as tsreader:
+				self.Tempscale = (float(tsreader.read().replace('\n', '')))
+			self.temp = ((self.temp)*(self.Tempscale))
+		except:
+			print "Barometer is not plugged in!"
+		finally:
+			return ((self.temp)*1.8+32) if "f" in mode else (self.temp)
 
 	def getPressure(self): # Return raw data which is (kPA) a form of pressure measurments sea level is about 100 
-		with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_pressure_raw", "r") as preader:
-			self.pressure = (float(preader.read().replace('\n', '')))
-		with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_pressure_scale", "r") as psreader:
-			self.Tempress = (float(psreader.read().replace('\n', '')))
-		return float((self.pressure)*(self.Tempress))
+		try:
+			with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_pressure_raw", "r") as preader:
+				self.pressure = (float(preader.read().replace('\n', '')))
+			with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_pressure_scale", "r") as psreader:
+				self.Tempress = (float(psreader.read().replace('\n', '')))
+		except:
+			print "Barometer is not plugged in!"
+		finally:
+			return float((self.pressure)*(self.Tempress))
 		
 class Accel:
 	def __init__(self): 
@@ -165,15 +200,19 @@ class Accel:
 			print "Error: No Accel detected"
 
 	def get(self): # Return accel data in array
-		with open("/sys/class/misc/FreescaleAccelerometer/data", "r") as reader:
-			self.raw= str(reader.read().replace('\n', ''))
-		for a in range(0, 3):
-			try:
-				self.accel[a] = (int(self.raw[0:self.raw.index(',')]) if ',' in self.raw else int(self.raw))
-				self.raw = self.raw[self.raw.index(',')+1:]
-			except:
-				break
-		return self.accel # return like this [x, y, z] in integer formats
+		try:
+			with open("/sys/class/misc/FreescaleAccelerometer/data", "r") as reader:
+				self.raw= str(reader.read().replace('\n', ''))
+			for a in range(0, 3):
+				try:
+					self.accel[a] = (int(self.raw[0:self.raw.index(',')]) if ',' in self.raw else int(self.raw))
+					self.raw = self.raw[self.raw.index(',')+1:]
+				except:
+					break
+		except:
+			print "Error using accelerometer!"
+		finally:
+			return self.accel # return like this [x, y, z] in integer formats
 
 class Magno:
 	def __init__(self): 
